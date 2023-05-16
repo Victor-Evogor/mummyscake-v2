424bb1e6e2555705eb47c00d4fb91c556fef1b4f
Victor Evogor <victorevogor0001@gmail.com>
2023-05-16 21:26:31 +0100

    Fixed a bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { CssBaseline } from '@mui/material'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {App} from './App'
+import {App} from './App';
+import { FavoritesProvider } from './providers/FavoritesProvider';
+
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <CssBaseline/>
+    <FavoritesProvider>
     <App />
+    </FavoritesProvider>
   </StrictMode>,
 )
