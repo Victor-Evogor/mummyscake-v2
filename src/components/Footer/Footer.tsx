86324bace7350d27983979a4fe7f57ce650db42c
Victor Evogor <victorevogor0001@gmail.com>
2023-05-31 21:28:37 +0100

    Made footer responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import {
   Typography,
   Stack,
   IconButton,
-  TableContainer,
+  TableContainer as TableCon,
   Table,
   TableHead,
   TableBody,
@@ -13,8 +13,10 @@ import {
   ListItem,
   List,
   Link,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, styled } from "@mui/system";
 import {
   FacebookOutlined,
   Twitter,
@@ -24,10 +26,25 @@ import {
   LinkedIn,
 } from "@mui/icons-material";
 
+const TableContainer = styled(TableCon)({
+  overflowX: "auto", // Enable horizontal scrolling for small screens
+  maxWidth: "100%", // Make the table container responsive
+});
+
 export const Footer = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // Set breakpoint for mobile screens
+
   return (
     <Box>
-      <Toolbar>
+      <Toolbar
+        sx={{
+          flexDirection: {
+            xs: "column",
+            md: "row",
+          },
+        }}
+      >
         <Typography variant="h6">
           Get Connected with Us on All Social Networks
         </Typography>
@@ -54,203 +71,417 @@ export const Footer = () => {
         </Stack>
       </Toolbar>
       <Divider />
-      <Toolbar>
-        <TableContainer component={"div"}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  sx={{
-                    borderBottom: 0,
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                  >
-                    Mummy&apos;s Cake
-                  </Typography>
-                </TableCell>
-                <TableCell
-                  sx={{
-                    borderBottom: 0,
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                    align="right"
-                  >
-                    Products
-                  </Typography>
-                </TableCell>
-                <TableCell
-                  sx={{
-                    borderBottom: 0,
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                    align="right"
-                  >
-                    Useful Links
-                  </Typography>
-                </TableCell>
-                <TableCell
-                  sx={{
-                    borderBottom: 0,
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      fontWeight: "bold",
-                    }}
-                    align="right"
-                  >
-                    Contact
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow
-                sx={{
-                  verticalAlign: "initial",
-                }}
-              >
-                <TableCell
-                  sx={{
-                    maxWidth: "8rem",
-                  }}
-                >
-                  <Typography variant="subtitle1">
-                    Satisfy your sweet tooth cravings with just a click -
-                    we&apos;ll deliver happiness right to your doorstep!
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <List disablePadding dense>
-                    <ListItem
-                      sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}
-                    >
-                      <Link href="#" underline="hover">Birthday Cakes</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Specialty Cakes</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Wedding Cakes</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">CupCakes</Link>
-                    </ListItem>
-                  </List>
-                </TableCell>
-                <TableCell>
-                  <List disablePadding dense>
-                    <ListItem
-                      sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}
-                    >
-                      <Link href="#" underline="hover">Pricing</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Settings</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Orders</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Help</Link>
-                    </ListItem>
-                  </List>
-                </TableCell>
-                <TableCell>
-                  <List disablePadding dense>
-                    <ListItem
-                      sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}
-                    >
-                      <Link href="tel:+2349133240345" underline="hover">+234 913 324 0345</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Satellite Town Road II, Calabar Nigeria</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">info@mummyscake.com</Link>
-                    </ListItem>
-                    <ListItem sx={{
-                        px: 0,
-                        display: "flex",
-                        justifyContent: "flex-end"
-                      }}>
-                      <Link href="#" underline="hover">Careers</Link>
-                    </ListItem>
-                  </List>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Toolbar>
+      <Toolbar>{isMobile ? <FooterTableSm /> : <FooterTableMd />}</Toolbar>
       <Box bgcolor="primary.main">
-      <Toolbar sx={{
-          display: "grid",
-          placeItems: "center"
-        }}>
-        <Typography color="white.main" >&copy;2022 <Link href="https://linktr.ee/victorevogor" color="white.main" target="_blank" rel="noreferrer" underline="hover">Victor Evogor</Link></Typography>
-      </Toolbar>
+        <Toolbar
+          sx={{
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <Typography color="white.main">
+            &copy;2022{" "}
+            <Link
+              href="https://linktr.ee/victorevogor"
+              color="white.main"
+              target="_blank"
+              rel="noreferrer"
+              underline="hover"
+            >
+              Victor Evogor
+            </Link>
+          </Typography>
+        </Toolbar>
       </Box>
     </Box>
   );
 };
+
+const FooterTableSm = () => {
+  return (
+    <Stack>
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: "bold",
+        }}
+      >
+        Mummy&apos;s Cake
+      </Typography>
+      <Typography variant="subtitle1">
+        Satisfy your sweet tooth cravings with just a click - we&apos;ll deliver
+        happiness right to your doorstep!
+      </Typography>
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: "bold",
+        }}
+      >
+        Products
+      </Typography>
+      <List disablePadding dense>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Birthday Cakes
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Specialty Cakes
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Wedding Cakes
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            CupCakes
+          </Link>
+        </ListItem>
+      </List>
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: "bold",
+        }}
+      >
+        Useful Links
+      </Typography>
+      <List disablePadding dense>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="tel:+2349133240345" underline="hover">
+            +234 913 324 0345
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Satellite Town Road II, Calabar Nigeria
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            info@mummyscake.com
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Careers
+          </Link>
+        </ListItem>
+      </List>
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: "bold",
+        }}
+      >
+        Contact
+      </Typography>
+      <List disablePadding dense>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="tel:+2349133240345" underline="hover">
+            +234 913 324 0345
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Satellite Town Road II, Calabar Nigeria
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            info@mummyscake.com
+          </Link>
+        </ListItem>
+        <ListItem
+          sx={{
+            px: 0,
+          }}
+        >
+          <Link href="#" underline="hover">
+            Careers
+          </Link>
+        </ListItem>
+      </List>
+    </Stack>
+  );
+};
+
+const FooterTableMd = () => (
+  <TableContainer>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell
+            sx={{
+              borderBottom: 0,
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+            >
+              Mummy&apos;s Cake
+            </Typography>
+          </TableCell>
+          <TableCell
+            sx={{
+              borderBottom: 0,
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+              align="right"
+            >
+              Products
+            </Typography>
+          </TableCell>
+          <TableCell
+            sx={{
+              borderBottom: 0,
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+              align="right"
+            >
+              Useful Links
+            </Typography>
+          </TableCell>
+          <TableCell
+            sx={{
+              borderBottom: 0,
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+              }}
+              align="right"
+            >
+              Contact
+            </Typography>
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow
+          sx={{
+            verticalAlign: "initial",
+          }}
+        >
+          <TableCell
+            sx={{
+              maxWidth: "8rem",
+            }}
+          >
+            <Typography variant="subtitle1">
+              Satisfy your sweet tooth cravings with just a click - we&apos;ll
+              deliver happiness right to your doorstep!
+            </Typography>
+          </TableCell>
+          <TableCell>
+            <List disablePadding dense>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Birthday Cakes
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Specialty Cakes
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Wedding Cakes
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  CupCakes
+                </Link>
+              </ListItem>
+            </List>
+          </TableCell>
+          <TableCell>
+            <List disablePadding dense>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Pricing
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Settings
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Orders
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Help
+                </Link>
+              </ListItem>
+            </List>
+          </TableCell>
+          <TableCell>
+            <List disablePadding dense>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="tel:+2349133240345" underline="hover">
+                  +234 913 324 0345
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Satellite Town Road II, Calabar Nigeria
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  info@mummyscake.com
+                </Link>
+              </ListItem>
+              <ListItem
+                sx={{
+                  px: 0,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Careers
+                </Link>
+              </ListItem>
+            </List>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
