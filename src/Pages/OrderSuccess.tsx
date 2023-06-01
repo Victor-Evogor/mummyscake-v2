7203167e4622eeb052291964124521b1da2d1ae1
Victor Evogor <victorevogor0001@gmail.com>
2023-06-01 20:17:50 +0100

    Made Success order page responsive

--- src/Pages/OrderSuccess.tsx
+++ src/Pages/OrderSuccess.tsx
@@ -1,25 +1,37 @@
-import { Box, Typography } from "@mui/material"
-import { NavBar } from "../components/NavBar/NavBar"
-import { Footer } from "../components/Footer/Footer"
-import { useUser } from "../hooks/useUser"
-import { Navigate } from "react-router-dom"
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { NavBar } from "../components/NavBar/NavBar";
+import { Footer } from "../components/Footer/Footer";
+import { useUser } from "../hooks/useUser";
+import { Navigate } from "react-router-dom";
 import Lottie from "react-lottie";
-import messageSent from "../lotties/message-sent-successfully-plane.json"
+import messageSent from "../lotties/message-sent-successfully-plane.json";
 
 export const OrderSuccess = () => {
-    const {user} = useUser();
-    if(!user) return <Navigate to="/"/>
-    return    <Box sx={{ flexGrow: 1 }}>
-        <NavBar/>
-        <Box maxWidth={"30%"} sx={{
-            marginX: "auto"
-        }}>
-        <Lottie options={{
+  const { user } = useUser();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  if (!user) return <Navigate to="/" />;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <NavBar />
+      <Box
+        maxWidth={"30%"}
+        sx={{
+          marginX: "auto",
+        }}
+      >
+        <Lottie
+          options={{
             animationData: messageSent,
-            loop: false
-        }}/>
-        </Box>
-        <Typography variant="h3" align="center" marginY={2}>Your order has been made successfully and is on it&apos;s way</Typography> 
-        <Footer/>
+            loop: false,
+          }}
+        />
+      </Box>
+      <Typography variant={isMobile ? "h5" : "h3"} align="center" marginY={2}>
+        Your order has been made successfully and is on it&apos;s way
+      </Typography>
+      <Footer />
     </Box>
-}
+  );
+};
