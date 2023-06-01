eda05ff6e3a07316c90d72962531a6c124198769
Victor Evogor <victorevogor0001@gmail.com>
2023-06-01 18:51:45 +0100

    Fixed full name field

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -13,6 +13,8 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { FunctionComponent, useRef, useState } from "react";
@@ -21,8 +23,11 @@ import { MuiTelInput, matchIsValidTel } from "mui-tel-input";
 import { signInWithGoogle } from "../../firebase";
 import { toast } from "react-toastify";
 import { useMutation } from "@apollo/client";
-import { CREATE_NEW_USER, CreateNewUserType } from "../../gql/createNewUser.gql";
-import { useCart } from "../../hooks/useCart"
+import {
+  CREATE_NEW_USER,
+  CreateNewUserType,
+} from "../../gql/createNewUser.gql";
+import { useCart } from "../../hooks/useCart";
 import { quantifyCakes } from "../../utils/quantifyCake";
 
 interface Props {
@@ -36,11 +41,14 @@ interface Props {
 }
 
 export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const fullName = useRef<HTMLInputElement>(null);
   const [createNewUser] = useMutation<CreateNewUserType>(CREATE_NEW_USER);
   const { setCart } = useCart();
+  const [isFullNameValid, setIsFullNameValid] = useState(true);
 
   const [showPassword, setShowPassword] = useState(false);
   const [phone, setPhone] = useState("+234");
@@ -64,7 +72,7 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
     >
       <Container>
         <Typography
-          variant="h3"
+          variant={isMobile ? "h5" : "h3"}
           my={2}
           textAlign="center"
           sx={{
@@ -79,12 +87,13 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
           my={3}
           onSubmit={(e) => {
             e.preventDefault();
-            if(!matchIsValidTel(phone) && (type === "create account")){
+            if (!matchIsValidTel(phone) && type === "create account") {
               toast.error("Invalid Phone number", { hideProgressBar: true });
             }
             if (
               !passwordRef.current?.value ||
               !emailRef.current?.value ||
+              !isFullNameValid ||
               (type === "create account"
                 ? !fullName.current?.value || !matchIsValidTel(phone)
                 : false)
@@ -93,14 +102,14 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
             const cred = {
               password: passwordRef.current?.value,
               email: emailRef.current?.value,
-              fullName: fullName.current?.value,
+              fullName: fullName.current?.value.trim(),
               phone: phone,
             };
             onSubmit(cred);
           }}
         >
           <Grid container spacing={3}>
-            <Grid item xs={6}>
+            <Grid item md={6}>
               <TextField
                 required
                 placeholder="******"
@@ -113,7 +122,7 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
               />
             </Grid>
 
-            <Grid item xs={6}>
+            <Grid item md={6}>
               <FormControl sx={{ width: "100%" }}>
                 <InputLabel htmlFor="password">Password</InputLabel>
                 <OutlinedInput
@@ -137,20 +146,39 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
 
             {type === "create account" ? (
               <>
-                <Grid item xs={6}>
+                <Grid item md={6}>
                   <TextField
                     required
-                    placeholder=""
+                    placeholder="John Doe"
                     label="Full Name"
                     type="text"
                     inputRef={fullName}
                     sx={{
                       width: "100%",
                     }}
+                    error={!isFullNameValid}
+                    helperText={
+                      !isFullNameValid
+                        ? "Please enter your first name and your surname, e.g John Doe"
+                        : undefined
+                    }
+                    onChange={() => {
+                      if (!fullName.current) return;
+                      setIsFullNameValid(
+                        fullName.current.value.split(" ").length > 1
+                      );
+                    }}
+                    onBlur={() => {
+                      if (!fullName.current) return;
+                      fullName.current.value = fullName.current.value.trim();
+                      setIsFullNameValid(
+                        fullName.current.value.split(" ").length > 1
+                      );
+                    }}
                   />
                 </Grid>
 
-                <Grid item xs={6}>
+                <Grid item md={6}>
                   <MuiTelInput
                     value={phone}
                     onChange={handlePhoneChange}
@@ -167,7 +195,12 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
           <ButtonGroup
             sx={{
               marginY: 2,
+              marginX: {
+                xs: "auto",
+                md: 0,
+              },
             }}
+            orientation={isMobile ? "vertical" : "horizontal"}
           >
             <Button variant="contained" type="submit">
               {type}
@@ -175,29 +208,42 @@ export const AuthForm: FunctionComponent<Props> = ({ type, onSubmit }) => {
             <Button
               startIcon={<Google />}
               onClick={() => {
-                signInWithGoogle().then(({user: {uid}})=>{
-                  createNewUser({
-                    variables: {
-                      userId: uid
-                    }
-                  }).then(({data})=>{
-                    if(!data) return
-                    const quantifiedCakes = quantifyCakes(data.createUser.cart.items)
-                    setCart(quantifiedCakes.map(({name, price, quantity, id}) => ({name, price, quantity, id})))
-                  })
-                }).catch(({ message }: Error) => {
-                  if (
-                    message === "Firebase: Error (auth/popup-closed-by-user)."
-                  ) {
-                    toast.error("Pup up closed unexpectedly", {
-                      hideProgressBar: true,
+                signInWithGoogle()
+                  .then(({ user: { uid } }) => {
+                    createNewUser({
+                      variables: {
+                        userId: uid,
+                      },
+                    }).then(({ data }) => {
+                      if (!data) return;
+                      const quantifiedCakes = quantifyCakes(
+                        data.createUser.cart.items
+                      );
+                      setCart(
+                        quantifiedCakes.map(
+                          ({ name, price, quantity, id }) => ({
+                            name,
+                            price,
+                            quantity,
+                            id,
+                          })
+                        )
+                      );
                     });
-                  } else {
-                    toast.error(
-                      "An unexpected error ocurred, please check your internet connection"
-                    );
-                  }
-                });
+                  })
+                  .catch(({ message }: Error) => {
+                    if (
+                      message === "Firebase: Error (auth/popup-closed-by-user)."
+                    ) {
+                      toast.error("Pup up closed unexpectedly", {
+                        hideProgressBar: true,
+                      });
+                    } else {
+                      toast.error(
+                        "An unexpected error ocurred, please check your internet connection"
+                      );
+                    }
+                  });
               }}
             >
               Continue With Google
